cce618ccab12a71fd5516c23649f1475f92cbb0b
Mahdi Jedari <i.jedari@gmail.com>
2020-12-31 13:24:47 +0330

    Updates

---
+++ precache-manifest.b80efff0a998d89320f65cb33fe3765e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd95791e16e743ebbe513d8b9d49ff62",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "15c43ff9761c16362157",
+    "url": "/portfolio/static/css/main.e2473e87.chunk.css"
+  },
+  {
+    "revision": "c6359446bc64555e6408",
+    "url": "/portfolio/static/js/2.9297f1d3.chunk.js"
+  },
+  {
+    "revision": "15c43ff9761c16362157",
+    "url": "/portfolio/static/js/main.51cadf4a.chunk.js"
+  },
+  {
+    "revision": "3593b2f94172585f4863",
+    "url": "/portfolio/static/js/runtime-main.3b58c541.js"
+  }
+]);
